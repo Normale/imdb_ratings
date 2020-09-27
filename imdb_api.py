72d75cc554c184eeaed70979d922b02312da6c5c
Normal <bartek.dybcio1@gmail.com>
2020-09-27 02:05:20 +0200

    api usage optimization

--- imdb_api.py
+++ imdb_api.py
@@ -2,12 +2,19 @@ from requests import get, post
 from typing import Tuple, List
 import json
 
+DEFAULTKEY = "fd10716c"
+
+def get_json(*args, **kwargs):
+    response = get(*args, **kwargs)
+    parsed = json.loads(response.text)
+    return parsed
+
+
 def search_title(title: str, key: str) -> Tuple[str, int]:
-    response_search = get('http://www.omdbapi.com/', params={
+    parsed = get_json('http://www.omdbapi.com/', params={
         't': f"{title}",
         'ApiKey': key
     })
-    parsed = json.loads(response_search.text)
     try:
         id: str = parsed["imdbID"]
         no_seasons: int = int(parsed["totalSeasons"])
@@ -17,20 +24,19 @@ def search_title(title: str, key: str) -> Tuple[str, int]:
     return id, no_seasons
 
 
-
-def get_episodes_data(id: str, no_seasons: int, key: str="fd10716c"):
+def get_episodes_data(id: str, no_seasons: int, key: str=DEFAULTKEY):
     data = []
     for season_nr in range(1, no_seasons + 1):
-        response = get('http://www.omdbapi.com/', params={
-            'i': id,
-            'Season': season_nr,
-            'ApiKey': key
-        })
-        parsed = json.loads(response.text)
+        parsed = get_json('http://www.omdbapi.com/', params={
+                'i': id,
+                'Season': season_nr,
+                'ApiKey': key
+            })
         data.append(parsed["Episodes"])
     return data
 
-def get_series_data(title: str, api_key: str="fd10716c") -> List[List[dict]]:
+
+def get_series_data(title: str, api_key: str=DEFAULTKEY) -> List[List[dict]]:
     try:
         id, no_seasons = search_title(title, api_key)
         print(id, no_seasons)
@@ -45,6 +51,7 @@ def get_series_data(title: str, api_key: str="fd10716c") -> List[List[dict]]:
     return result
 
 
+
 if __name__ == "__main__":
     key = "fd10716c"
     data = []
@@ -52,4 +59,3 @@ if __name__ == "__main__":
     id, no_seasons = search_title(question,key)
     data = get_episodes_data(id, no_seasons, key)
     print(data)
-    
